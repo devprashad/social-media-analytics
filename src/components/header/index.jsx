1e7692857816e11a8b8af0e3e68141f8bb54a85c
mdxaasil <mdaasil18@gmail.com>
2024-05-01 13:10:20 +0530

    Update logged in logic

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,20 +3,21 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/authContext'
 import { doSignOut } from '../../firebase/auth'
 
+
 const Header = () => {
     const navigate = useNavigate()
     const { userLoggedIn } = useAuth()
+
     return (
         <nav className='flex flex-row justify-between w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
-            <div>
-                <Link className='text-sm text-blue-600 underline mr-4' to={'/Youtube'}>Youtube</Link>
-                <Link className='text-sm text-blue-600 underline mr-4' to={'/Youtube'}>Youtube</Link>
-                <Link className='text-sm text-blue-600 underline mr-4' to={'/Youtube'}>Youtube</Link>
-
-            </div>
+            {userLoggedIn && (
+                <div>
+                    <Link className='text-sm text-blue-600 underline mr-4' to={'/Youtube'}>Youtube</Link>
+                </div>
+            )}
             <div>
                 {userLoggedIn ?
-                    <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline'>Logout</button>
+                    <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline ml-2'>Logout</button>
                     :
                     <>
                         <Link className='text-sm text-blue-600 underline mr-4' to={'/login'}>Login</Link>
